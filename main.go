ab86d78f9e4e28778fc0abfca5b7d575a7248af4
DemuraAIdev <cyberaioff@gmail.com>
2024-11-07 16:20:54 +0700

    Refactor Discord API and add authentication middleware

--- main.go
+++ main.go
@@ -14,12 +14,17 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
+	router.Use(api.AuthMiddleware())
+
 	// Spotify api
 	router.GET("/util/spotify/now-playing", api.GetNowPlaying)
 	router.GET("/util/spotify/recently-played", api.GetRecentlyPlayed)
 
 	// Heartbeat api
 	router.GET("/heartbeat", api.Heartbeat)
+
+	// Discord api
+	router.GET("/core/discord/me", api.GetMeDetails)
 
 	router.Run("0.0.0.0:3000")
 }
